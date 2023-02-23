c7f97e7cae360e6206658013aef2b9579fb325f7
Sourabh Sagar <sourabhsagar7601@gmail.com>
2023-02-23 22:31:47 +0530

    Blob to Commit-tree, lifecycle via bash
    Understanding life-cycle of commit from blob to commit
    via rev-parse, write-tree, commit-tree, cat-file, ls-files, od, etc

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ###################################################################################################
-Desc: 
+Desc:
 	Some pre-made scripts to deep dive git, covering topic such as lifeOfCommit.sh, conflict,
-	rebase, bisect, submodule. Suggested order to understand:- 
+	rebase, bisect, submodule. Suggested order to understand:-
 	*  lifeOfCommit.sh
 	1) conflict.sh
 	   conflict2.sh
@@ -15,8 +15,8 @@ Desc:
 
 Que. What does git init do ?
 	Before		|	After
-------------------------------------------------------					
-			|  .git/config	
+------------------------------------------------------
+			|  .git/config
 			|  .git/description
 			|  .git/HEAD
 			|  .git/hooks
@@ -27,26 +27,26 @@ Que. What does git init do ?
 Que. What happens when i git add ?
 
 	Before		|	After
-------------------------------------------------------					
-  .git/config		|  ...all those, and: 
+------------------------------------------------------
+  .git/config		|  ...all those, and:
   .git/description	|  .git/index
   .git/HEAD		|  .git/3d/16dec17e094468d6
   .git/hooks		|
-  .git/info/exclude	|  
+  .git/info/exclude	|
 
 
 
 Que. What is a commit ?
 
 	Before			|	After
------------------------------------------------------------------------					
-  .git/index			|  .git/index --updated 
+-----------------------------------------------------------------------
+  .git/index			|  .git/index --updated
   .git/3d/16dec17e094468d6	|  .git/COMMIT_EDITMSG
   .git/HEAD			|  .git/logs/HEAD
   ...				|  .git/logs/refs/heads/master
 				|  .git/refs/heads/master
 				|  ...all the others + more objects
-	
+
 
 
 ..............cd inside shell don't work because................
@@ -55,6 +55,6 @@ Que. What is a commit ?
 3) that's why the curretn directory doesn't change
 
 Sol:- to achive changing of directory via shell script use sourcing of script
-for eg:- 
+for eg:-
 . scriptname.sh
 source scriptname.sh
--- scripts/lifeOfCommit.sh
+++ scripts/lifeOfCommit.sh
@@ -2,13 +2,13 @@
 
 function git_init()
 {
+	echo '*********************** Git Init ****************************'
 	set -x
-	mkdir -p lifeOfCommit/.git
+	mkdir -p lifeOfCommit/.git/refs
 	cd lifeOfCommit
-	mkdir -p .git/objects
-	mkdir -p .git/refs
+	mkdir -p .git/objects/{info,pack}
 	echo -e "[core]\n\trepositoryformatversion = 0\n\tfilemode = true\n\t bare = false\n\tlogallrefupdates = true" > .git/config
-	git status 
+	git status
 	if [ $? -ne 0 ]
 	then
 		echo 'ref: refs/heads/main' > .git/HEAD
@@ -17,4 +17,95 @@ function git_init()
 	set +x
 }
 
+# accepts blob id and fileSize then create zlib compressed file in .git/objects/01/23fdlakfja...
+function blobObjCreate()
+{
+	echo '*********************** Blob Object Create ****************************'
+	tree -a .git/
+	tFileSize=$2
+	tBlobId=$1; dirName=${tBlobId:0:2}
+	blobFileName=$(echo $tBlobId | cut -c 3-)
+	mkdir -p .git/objects/$dirName
+	cd .git/objects/$dirName
+	{ printf 'blob %u\0' $tFileSize; cat ../../../main.c; } | pigz -zc > $blobFileName
+	chmod -w $fileName
+	cd ../../../
+	tree -a .git/
+	git ls-files --debug
+	read
+	unset tBlobId tFileSize dirName blobFileName
+}
+
+function update-index()
+{
+	echo '*********************** Upate Index ****************************'
+	tBlobId=$1; tFileName=$2
+	git status; git ls-files --debug
+	git update-index --add --cacheinfo 100644 $tBlobId $tFileName
+	git ls-files --debug
+	tree -a .git/
+	read
+
+	git status
+	git ls-files --debug
+	read
+	unset tBlobId tFileName
+}
+
+
+function show_index()
+{
+	echo '*********************** Show Index ****************************'
+	od -tx1z .git/index
+	git ls-files --debug
+	read
+}
+
+function create_commit()
+{
+	echo '*********************** Create Commit ****************************'
+	treeId=$(git write-tree)
+	git ls-tree $treeId
+	tree -a .git/
+	git cat-file tree $treeId | od -tx1z
+	read
+
+	commitId=$(git commit-tree $treeId -m 'Deep Dive Git with ssagar')
+	git show
+	if [ $? -ne 0 ]
+	then
+		tree -a .git
+		git update-ref refs/heads/main $commitId
+		read
+		tree -a .git
+	fi
+	git show
+	read
+
+	git cat-file --batch-check --batch-all-objects
+	git cat-file commit $commitId
+	pigz -dcz < .git/objects/${commitId:0:2}/${commitId:2:${#commitId}}
+
+}
+
 git_init
+echo '*********************** Creating/Deleting/Modifying content ****************************'
+set -x
+fileName=main.c
+echo -e "#include <stdio.h>\n\nint main(){\n\tprintf(\"Hello World\\\n\");\n}" > $fileName
+ls -l
+read
+
+fileSize=$(wc -c main.c | awk '{ print $1 }')
+blobId=$({ printf 'blob %u\0' $fileSize; cat $fileName; } | sha1sum | awk '{ print $1 }')
+read
+
+blobObjCreate $blobId $fileSize
+git fsck
+read
+
+update-index $blobId $fileName
+show_index
+create_commit
+read
+set +x
